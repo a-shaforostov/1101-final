48bf63e1e535bfb5af5d1aa53ca2e49e10333bbf
andrii.shaforostov <andpr99@gmail.com>
2018-11-17 18:01:45 +0200

    sent version

--- server/app.js
+++ server/app.js
@@ -1,11 +1,6 @@
 var os = require('os');
 const addrs = require('local-ip-addresses-and-hostnames');
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// const cookieParser = require('cookie-parser');
 const WebSocket = require('ws');
-// const indexRouter = require('./routes/index');
 const sessions = require('./sessions');
 
 const wss = new WebSocket.Server({
@@ -13,17 +8,6 @@ const wss = new WebSocket.Server({
 });
 
 const wsHandlers = require('./wsHandlers')(wss);
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
 
 wss.on('connection', (ws) => {
   sessions.addConnection(ws);
@@ -34,7 +18,6 @@ wss.on('connection', (ws) => {
 });
 
 function processMessage(message) {
-  // console.log(message);
   const { action, payload } = JSON.parse(message);
   wsHandlers[action](this, payload);
 }
